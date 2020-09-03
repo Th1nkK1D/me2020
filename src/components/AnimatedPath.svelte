30fb4dd3f855fae26027815cdb437850665d7237
Th1nkK1D <witheep@gmail.com>
2020-09-03 13:12:16 +0700

    Replace SMIL animation with animejs

---
+++ src/components/AnimatedPath.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import anime from 'animejs/lib/anime.es.js'
+  import { onMount } from 'svelte'
+
+  export let duration: number
+  export let startValue: string
+  export let endValue: string
+  export let fill: string
+
+  let path
+
+  onMount(() => {
+    anime({
+      targets: path,
+      d: [startValue, endValue],
+      easing: 'easeInOutQuad',
+      duration,
+      loop: true,
+      direction: 'alternate',
+    })
+  })
+</script>
+
+<path bind:this="{path}" {fill} d="{startValue}"></path>

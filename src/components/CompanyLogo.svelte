b26895f0940b09d1b5506607773f05c92e2f1e2b
Th1nkK1D <witheep@gmail.com>
2020-09-23 21:12:01 +0700

    Update work experience design

--- src/components/CompanyLogo.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  export let name: string
-  export let image: string
-  export let url: string
-</script>
-
-<a href="{url}" target="_blank">
-  <img src="{`./images/${image}`}" alt="{name}" class="w-10" />
-</a>
--- src/sections/WorkExperience.svelte
+++ src/sections/WorkExperience.svelte
@@ -1,49 +1,61 @@
 <script lang="ts">
   import Heading2 from '../typography/Heading2.svelte'
-  import CompanyLogo from '../components/CompanyLogo.svelte'
-  import WorkItemContainer from '../components/WorkItemContainer.svelte'
+  import WorkItem from '../components/WorkItem.svelte'
+
+  const EXPERIENCE_START_YEAR = 2018
+  const yearsOfTotalExperiences =
+    new Date().getFullYear() - EXPERIENCE_START_YEAR
+
+  const works = [
+    {
+      company: 'Thoughtworks',
+      position: 'Developer Consultant',
+      period: 'Aug 2019 - Aug 2020',
+      image: 'thoughtworks.jpg',
+      link: 'https://www.thoughtworks.com',
+    },
+    {
+      company: 'Boonme Lab',
+      position: 'Freelance',
+      period: 'Apr - Jun 2019',
+      image: 'boonmelab.png',
+      link: 'https://www.boonmeelab.com',
+    },
+    {
+      company: 'H-Lab Consulting',
+      position: 'Freelance',
+      period: 'Aug 2017 - Nov 2018',
+      image: 'hlab.png',
+      link: 'https://www.hlabconsulting.com',
+    },
+    {
+      company: 'Wongnai Media',
+      position: 'Intern',
+      period: 'Jun - Aug 2018',
+      image: 'wongnai.png',
+      link: 'https://www.wongnai.com"',
+    },
+    {
+      company: 'ActiMotive',
+      position: 'Freelance',
+      period: 'Jul 2017 - May 2018',
+      image: 'actimotive.png',
+      link: 'https://www.acti-motive.com',
+    },
+  ]
 </script>
 
 <section>
   <Heading2>Work Experience</Heading2>
-  <div class="grid gap-4 md:gap-6 md:grid-cols-11">
-    <div class="md:col-span-4">
-      <WorkItemContainer heading="Full-time developer consultant" year="{1}">
-        <CompanyLogo
-          name="ThoughtWorks (Thailand)"
-          image="thoughtworks.jpg"
-          url="https://www.thoughtworks.com"
-        />
-      </WorkItemContainer>
-    </div>
-    <div class="md:col-span-4">
-      <WorkItemContainer heading="Freelance developer" year="{2}">
-        <CompanyLogo
-          name="Boonme Lab"
-          image="boonmelab.png"
-          url="https://www.boonmeelab.com"
-        />
-        <CompanyLogo
-          name="H Lab Consulting"
-          image="hlab.png"
-          url="https://www.hlabconsulting.com"
-        />
-        <CompanyLogo
-          name="ActiMotive"
-          image="actimotive.png"
-          url="https://www.acti-motive.com"
-        />
-      </WorkItemContainer>
-    </div>
-    <div class="md:col-span-3">
-      <WorkItemContainer heading="Developer intern">
-        <CompanyLogo
-          name="Wongnai Media"
-          image="wongnai.png"
-          url="https://www.wongnai.com"
-        />
-      </WorkItemContainer>
-    </div>
 
+  <div class="grid gap-4 md:gap-6 md:grid-cols-3">
+    {#each works as work}
+      <WorkItem {...work} />
+    {/each}
+
+    <div class="rounded-lg px-6 py-4 text-center md:text-left">
+      <p class="text-2xl font-bold">~{yearsOfTotalExperiences} years</p>
+      <p>of total experiences</p>
+    </div>
   </div>
 </section>

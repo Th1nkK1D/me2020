fcbff81924ba419bd5df66ef6b0a763d46ba83ba
Th1nkK1D <witheep@gmail.com>
2021-12-12 22:01:01 +0200

    Update showcase projects

--- src/sections/Showcase.svelte
+++ src/sections/Showcase.svelte
@@ -4,6 +4,47 @@
 
   const showcaseItems = [
     {
+      title: 'OrdinaryUnfold',
+      description:
+        'My solo hobby project for visualization and storytelling experiments',
+      image: 'ordinary-unfold.png',
+      link: 'https://www.ordinaryunfold.com/',
+    },
+    {
+      title: 'CONstitution LAB',
+      description:
+        'Interactive quizes helping Thai citizens to understand and design the constitution',
+      image: 'conlab.png',
+      link: 'https://conlab.conlabth.com/',
+    },
+    {
+      title: '112 Story',
+      description:
+        'A storytelling about lèse-majesté law abusement in Thailand',
+      image: '112story.jpeg',
+      link: 'https://112story.com/',
+    },
+    {
+      title: 'ELECT: CDA',
+      description:
+        'A visualization about Constitution Drafting Assembly of Thailand',
+      image: 'cda.png',
+      link: 'https://elect.in.th/cda/',
+    },
+    {
+      title: 'ELECT: Civil Movements',
+      description: 'A visualization about Thai civil movements in 2020',
+      image: 'civil-movements.png',
+      link: 'https://elect.in.th/civil-movement-2020/',
+    },
+    {
+      title: 'ELECT: Reconstitution',
+      description:
+        'A visualization and articles collection about Thailand constitution',
+      image: 'recon.png',
+      link: 'https://recon.elect.in.th/',
+    },
+    {
       title: 'The Monument Garden',
       description: 'A relaxing gardening game, built with Godot and Blender',
       image: 'monumentgarden.png',
@@ -27,19 +68,6 @@
       description: 'An Interactive Quantum Computing Mini Exhibition',
       image: 'quantarium.png',
       link: 'https://github.com/Th1nkK1D/quantarium',
-    },
-    {
-      title: 'Swallom',
-      description: 'Nearby place-to-eat randomizer progressive web app',
-      image: 'swallom.jpg',
-      link: 'https://swallom-1506749334555.firebaseapp.com/',
-    },
-    {
-      title: 'VQE Battleship',
-      description:
-        'Variational Quantum Eigensolver for Battleship Puzzle with Qiskit research',
-      image: 'vqe.png',
-      link: 'https://github.com/Th1nkK1D/vqe-battleship',
     },
   ]
 </script>

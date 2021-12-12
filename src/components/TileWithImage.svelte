edaaaf7117c04730ccb1790e786a3ba9e60692ab
Th1nkK1D <witheep@gmail.com>
2021-12-12 21:02:19 +0200

    Update tile with image component

---
+++ src/components/TileWithImage.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let title: string
+  export let subtitle: string
+  export let description: string
+  export let image: string
+  export let link: string
+</script>
+
+<div class="rounded-lg bg-gray-100 px-4 py-3 flex flex-row space-x-4">
+  <div class="flex">
+    <a href="{link}" target="_blank" class="m-auto">
+      <img src="./images/{image}" alt="{title}" class="w-16 rounded-lg" />
+    </a>
+  </div>
+  <div class="flex-1 flex flex-col justify-center">
+    <p class="font-bold">{title}</p>
+    <p>{subtitle}</p>
+    <p class="text-xs text-gray-600">{description}</p>
+  </div>
+</div>
--- src/sections/WorkExperience.svelte
+++ src/sections/WorkExperience.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Heading2 from '../typography/Heading2.svelte'
-  import WorkItem from '../components/WorkItem.svelte'
+  import TileWithImage from '../components/TileWithImage.svelte'
 
   const EXPERIENCE_START_YEAR = 2018
   const yearsOfTotalExperiences =
@@ -8,44 +8,44 @@
 
   const works = [
     {
-      company: 'Punch Up',
-      position: 'Frontend Developer',
-      period: 'Nov 2020 - Now',
+      title: 'Punch Up',
+      subtitle: 'Frontend Developer',
+      description: 'Nov 2020 - Now',
       image: 'punchup.png',
       link: 'https://punchup.world/',
     },
     {
-      company: 'Thoughtworks',
-      position: 'Developer Consultant',
-      period: 'Aug 2019 - Aug 2020',
+      title: 'Thoughtworks',
+      subtitle: 'Developer Consultant',
+      description: 'Aug 2019 - Aug 2020',
       image: 'thoughtworks.jpg',
       link: 'https://www.thoughtworks.com',
     },
     {
-      company: 'Boonme Lab',
-      position: 'Freelance',
-      period: 'Apr - Jun 2019',
+      title: 'Boonme Lab',
+      subtitle: 'Freelance',
+      description: 'Apr - Jun 2019',
       image: 'boonmelab.png',
       link: 'https://www.boonmeelab.com',
     },
     {
-      company: 'H-Lab Consulting',
-      position: 'Freelance',
-      period: 'Aug 2017 - Nov 2018',
+      title: 'H-Lab Consulting',
+      subtitle: 'Freelance',
+      description: 'Aug 2017 - Nov 2018',
       image: 'hlab.png',
       link: 'https://www.hlabconsulting.com',
     },
     {
-      company: 'Wongnai Media',
-      position: 'Intern',
-      period: 'Jun - Aug 2018',
+      title: 'Wongnai Media',
+      subtitle: 'Intern',
+      description: 'Jun - Aug 2018',
       image: 'wongnai.png',
       link: 'https://www.wongnai.com"',
     },
     {
-      company: 'ActiMotive',
-      position: 'Freelance',
-      period: 'Jul 2017 - May 2018',
+      title: 'ActiMotive',
+      subtitle: 'Freelance',
+      description: 'Jul 2017 - May 2018',
       image: 'actimotive.png',
       link: 'https://www.acti-motive.com',
     },
@@ -56,16 +56,13 @@
   <Heading2>Work Experience</Heading2>
 
   <div class="grid gap-4 md:gap-6 md:grid-cols-3">
-    <div
-      class="rounded-lg px-6 py-4 text-center md:text-right flex flex-col
-      justify-center"
-    >
+    {#each works as work}
+      <TileWithImage {...work} />
+    {/each}
+
+    <div class="rounded-lg px-6 py-4 flex flex-col justify-center">
       <p class="text-2xl font-bold">~{yearsOfTotalExperiences} years</p>
       <p>of total experiences</p>
     </div>
-
-    {#each works as work}
-      <WorkItem {...work} />
-    {/each}
   </div>
 </section>
